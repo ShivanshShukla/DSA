7eec97c4d55d8e8fbfb21e24b4cc7e046d6d8b42
shivansh <shivanshshukla.3@gmail.com>
2024-03-30 02:20:10 +0530

    hashing solutions

---
+++ ATOZ_DSA/Hashing/CountFrequnce.java
@@ -0,0 +1,34 @@
+package Hashing;
+
+import java.util.HashMap;
+
+class Count {
+    public static int[] Frequency(int arr[], int n) {
+        int a[] = new int[n];
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i : arr) {
+            if (hm.containsKey(i)) {
+                hm.put(i, hm.getOrDefault(i, 0) + 1);
+            } else {
+                hm.put(i, 1);
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (hm.get(i + 1) != null) {
+                a[i] = hm.get(i + 1);
+            }
+        }
+        return a;
+    }
+}
+
+public class CountFrequnce {
+    public static void main(String[] args) {
+        int n = 6;
+        int arr[] = { 1, 3, 1, 9, 2, 7 };
+        int a[] = Count.Frequency(arr, n);
+        for (int i : a) {
+            System.out.println(i + " ");
+        }
+    }
+}
---
+++ ATOZ_DSA/Hashing/MaxnMinFreq.java
@@ -0,0 +1,46 @@
+package Hashing;
+
+import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+class Frequency {
+    public static int[] count(int arr[]) {
+        int n = arr.length;
+        HashMap<Integer, Integer> maparr = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (maparr.containsKey(arr[i])) {
+                maparr.put(arr[i], maparr.get(arr[i]) + 1);
+            } else {
+                maparr.put(arr[i], 1);
+            }
+        }
+        int maxfreq = 0, minfreq = n;
+        int maxele = 0, minele = 0;
+        for (Map.Entry<Integer, Integer> it : maparr.entrySet()) {
+
+            int count = it.getValue();
+            int element = it.getKey();
+
+            if (count > maxfreq || (count == maxfreq && element < maxele)) {
+
+                maxele = element;
+                maxfreq = count;
+            }
+            if (count < minfreq || (count == minfreq && element < minele)) {
+                minele = element;
+                minfreq = count;
+            }
+        }
+        return new int[] { maxele, minele };
+    }
+}
+
+public class MaxnMinFreq {
+    public static void main(String[] args) {
+        int arr[] = { 10, 10, 10, 3, 3, 3 };
+        int ar[] = Frequency.count(arr);
+        System.out.println(ar[0] + " " + arr[1]);
+    }
+}
